411878baf2adc0a3813d47b21e7a5ab0c45be620
schplitt <schplitt.contact@gmail.com>
2025-02-27 19:23:53 +0100

    improve description

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # MDC Nuxthub rendering bug repo
 
-As described in the issue, there seems to be a bug when client side rendering Markdown content using the `@nuxtjs/mdc` module.
+As described in the issue, there seems to be a bug when client side rendering Markdown content using the [`@nuxtjs/mdc`](https://github.com/nuxt-modules/mdc) module.
 
-Rendering works fine on the server side, but when navigating or refreshing the page, the content is not rendered.
+Rendering works fine in dev and on SSR, but during client side rendering, the content is not rendered.
 
 You can  deploy this repo via this button to NuxtHub to see the issue in action: 
 
 [![Deploy to NuxtHub](https://hub.nuxt.com/button.svg)](https://hub.nuxt.com/new?repo=schplitt/mdc_nuxthub_repro)
 
-Or visit the deployed version here: [https://mdc-nuxthub-repro.nuxt.dev/](https://mdc-nuxthub-repro.nuxt.dev/)
+Or visit the deployed version here: [https://mdc-nuxthub-reproduction.nuxt.dev/](https://mdc-nuxthub-reproduction.nuxt.dev/)
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,44 @@
 <script setup lang="ts">
+// Markdown content to be rendered
 const md = `
 # Hello MDC
 
-::alert
-Hello MDC
-::
 
 - List 1
 - List 2
 - List 3
 `
+
+// Parse the markdown content
+const { data: ast, error } = await useAsyncData('markdown', () => parseMarkdown(md))
+
+// Watch for errors during parsing
+watchEffect(() => {
+  if (error.value) {
+    console.error(error.value)
+  }
+})
 </script>
 
 <template>
-  <MDC :value="md" tag="article" />
+  <div>
+    <!-- Render markdown content using MDC component -->
+    <span>
+      Using MDC to render markdown content with `# Hello MDC`
+    </span>
+    <MDC :value="md" tag="article" />
+
+    <!-- Render markdown content using MDCRenderer component -->
+    <pre>
+      Using MDCRenderer to render markdown content with:
+
+      const { data: ast, error } = await useAsyncData('markdown', () => parseMarkdown('md'))
+    </pre>
+
+    <MDCRenderer v-if="ast" :body="ast.body" :data="ast.data" />
+
+    <span>
+      Take a look at the console for errors
+    </span>
+  </div>
 </template>
